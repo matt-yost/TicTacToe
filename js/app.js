0b4affe13fd4a4b490a9fdb9af9d475c35ac3c3e
Matt Yost <matt.yost2@gmail.com>
2016-09-17 15:05:23 -0500

    Fixed hover
    Hover image matches active player

--- js/app.js
+++ js/app.js
@@ -1,10 +1,67 @@
-'use strict';
+(function(){
 
-// Initially hide playing board
-$('#board').hide();
+  'use strict';
 
+  // Initially hide playing board
+  $('#board').hide();
 
-$('#start a').click(function(){
-  $('#start').hide();
-  $('#board').show();
-});
+  var playerList = $("#board header ul li");
+  var activePlayer;
+  // Identify active player
+  var getActivePlayer = function(){
+    playerList.each(function(){
+      if($(this).hasClass("active")){
+        activePlayer = $(this);
+      }
+    });
+  };
+  // On hover, show X or O according to active player
+
+  $(".box").mouseenter(function(){
+    getActivePlayer();
+    if(activePlayer.attr("id") === "player1"){
+      $(this).css("background-image", "url(./img/o.svg)");
+    } else {
+      $(this).css("background-image", "url(./img/x.svg)");
+    }
+  });
+
+  $(".box").mouseleave(function(){
+    $(this).css("background-image", "none");
+  });
+
+  var playCount = 0;
+
+  $('#start a').click(function(){
+    // Hide start screen and show game borad
+    $('#start').hide();
+    $('#board').show();
+
+    // Set player1 to active
+    $("#player1").toggleClass('active');
+    getActivePlayer();
+  });
+
+  // Remove hover bind and keep players choice on board
+  $(".boxes li").click(function(){
+    getActivePlayer();
+    $(this).unbind('mouseenter mouseleave');
+    if(activePlayer.attr("id") === "player1"){
+      $(this).addClass("box-filled-1");
+      playCount ++;
+    } else if(activePlayer.attr("id") === "player2"){
+      $(this).addClass("box-filled-2");
+      playCount++;
+    }
+
+    if(playCount % 2 !== 0){
+      $("#player2").toggleClass("active");
+      $("#player1").toggleClass("active");
+    } else {
+      $("#player1").toggleClass("active");
+      $("#player2").toggleClass("active");
+    }
+  });
+
+// Do not venture past this point
+}());
